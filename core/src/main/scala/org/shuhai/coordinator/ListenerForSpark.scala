d9c0d39f2ee40206838173c0ce1d41c2caab8240
Xinjinhan <18183124928@163.com>
2022-05-30 21:18:54 +0800

    first commit

---
+++ core/src/main/scala/org/shuhai/coordinator/ListenerForSpark.scala
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.shuhai.coordinator
+
+import scala.util.Properties
+
+import org.apache.spark.SparkConf
+import org.apache.spark.internal.Logging
+import org.apache.spark.scheduler.{SparkListener, SparkListenerApplicationStart, SparkListenerTaskEnd, SparkListenerTaskStart}
+
+import java.io.FileWriter
+
+class ListenerForSpark(conf: SparkConf) extends SparkListener with Logging {
+
+  val recordElapsedTime = 10
+  var currentDuration = 0
+  var runningTaskNum = 0
+  val sparkHome = Properties.envOrElse("SPARK_HOME","/home/root" )
+
+  override def onApplicationStart(applicationStart: SparkListenerApplicationStart): Unit = {
+    logInfo("Coordinator: Applicaiton is staertd")
+    val p = new Printer()
+    p.start()
+  }
+
+  override def onTaskStart(taskStart: SparkListenerTaskStart): Unit = {
+    val taskId = taskStart.taskInfo.taskId
+    runningTaskNum += 1
+    print(s"task Id is $taskId\n")
+  }
+
+  override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
+    val taskId = taskEnd.taskInfo.taskId
+    runningTaskNum -= 1
+    print(s"task Id is $taskId\n")
+  }
+
+  class Printer extends Thread {
+    override def run(): Unit = {
+      val taskParallelismFile = new FileWriter(s"$sparkHome/taskParallelism.csv",true)
+      while (true) {
+        Thread.sleep(recordElapsedTime)
+        currentDuration += recordElapsedTime
+        taskParallelismFile.write(s"$currentDuration,$runningTaskNum\n")
+        print(s"current is $runningTaskNum\n")
+      }
+      taskParallelismFile.close()
+    }
+  }
+}
